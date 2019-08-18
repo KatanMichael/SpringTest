6ac42cb48c36509e727dd2840e9a075efe7ed84f
Michael Katan <michaelkatan@gmail.com>
2019-08-18 22:28:38 +0300

    added new path

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,20 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name)
+    {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    @RequestMapping("/randomPerson")
+    public Person randomPerson(@RequestParam(value="name", defaultValue = " ") String name)
+    {
+        Random random = new Random();
+        Person p = new Person(random.nextInt(1000),random.nextInt(120),name);
+        return p;
+    }
+
+
+
 }
